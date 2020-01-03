136e5bb663e1f9ea8d36014072201bfb10cf5543
宋广贤 <ll@pigsong-mb0.tencent.com>
2020-01-03 16:24:12 +0800

    python2 and python3

--- auth.py
+++ auth.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python2.7
+#!/usr/bin/env python
 
 import sqlite3
 import os
 import sys
 
+py_ver = sys.version_info.major
 username = os.getenv("username")
 password = os.getenv("password")
 if username is None:
@@ -14,11 +15,15 @@ conn = sqlite3.connect('auth.db')
 c = conn.cursor()
 for row in c.execute("select password from auth where username='"+username+"'"):
 
-	print username
-	print password
-	print row[0]
-	if cmp(password,row[0]):
-		sys.exit(1)
-	
+	print (username)
+	print (password)
+	print (row[0])
+	if (py_ver == 2):
+		if cmp(password,row[0]):
+			sys.exit(1)
+	if (py_ver == 3):
+		import operator
+		if operator.ne(password,row[0]):
+			sys.exit(1)
 	sys.exit(0)
 sys.exit(1)
